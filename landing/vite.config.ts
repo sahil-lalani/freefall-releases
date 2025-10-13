c531f6c57130b7b5796621f316e58de0689b7976
Sahil Lalani <99920148+sahil-lalani@users.noreply.github.com>
2025-10-13 10:53:27 -0700

    removed landing

--- landing/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/journal/',  // 👈 must match the repo name
-})
